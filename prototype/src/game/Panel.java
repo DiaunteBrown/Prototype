111fc4d80a44e0ca04870ba45d4fd82b7869187a
DBrown22641 <DBrown22641@S-DBROWN22641.center.loc>
2019-11-15 11:45:59 -0600

    ???

--- prototype/src/game/Panel.java
+++ prototype/src/game/Panel.java
@@ -22,7 +22,7 @@ public class Panel extends JPanel implements KeyListener {
 	
 	public Panel() {
 		addKeyListener(this);
-		setBackground(new Color(64, 64, 64));
+		setBackground(new Color(0, 0, 0));
 		setDoubleBuffered(true);
 		setFocusable(true);
 		
@@ -35,15 +35,13 @@ public class Panel extends JPanel implements KeyListener {
 	}
 	
 	public void drawSplashPage(Graphics g) {
-		g.setColor(Color.MAGENTA);
 		//Title Screen//
-		
 		g.setColor(Color.red);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 90)); 
 		//Title//
 		g.drawString("Fidgy-Rubiks", 150, 250);
 		//Loading Bar Place holder//
-		g.fillRect(330, 365, 150, 20);
+		g.fillRect(330, 365, 60, 20);
 		g.drawRect(330, 365, 150, 20);
 		//Loading//
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30)); 
@@ -54,9 +52,30 @@ public class Panel extends JPanel implements KeyListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		drawSplashPage(g);
-
+		//drawSplashPage(g);
+		SecondScreen(g);
 	}
+	public void SecondScreen(Graphics g) {
+		//Rubiks Cube Menu//
+		
+		g.setColor(Color.red);
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
+		g.drawString("Game Mode", 230, 50 );
+		
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 30)); 
+
+		g.drawString("Beginner", 300, 150);
+		
+		g.drawString("Intermediate", 300, 250);
+		
+		g.drawString("Profesional", 300, 350);
+		
+		g.drawImage(new ImageIcon("src/pictures/Shopping Cart 2.png").getImage(), 650, 10, 200, 100, null);
+		g.drawImage(new ImageIcon("src/pictures/Menu Bar.png").getImage(), -50, 0, 150, 100, null);
+		
+		
+	}
+	
 
 	@Override
 	public void keyPressed(KeyEvent k) {
