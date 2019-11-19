ee98b975f454c2971484b2acc76c6b12b75cacdc
DBrown22641 <DBrown22641@S-DBROWN22641.center.loc>
2019-11-19 10:28:05 -0600

    ??

--- prototype/src/game/Panel.java
+++ prototype/src/game/Panel.java
@@ -19,14 +19,25 @@ import javax.swing.JPanel;
 
 public class Panel extends JPanel implements KeyListener {
 
+	
+	///////////////////////////////////////////////////
+	
+	
 	String screenTracker;
+	String[] Screen = {"Title_screen","selection_screen"};
+	int screenListIndex=0;
+	
+	
+	
+	///////////////////////////////////////////////////
 	
 	public Panel() {
 		addKeyListener(this);
-		setBackground(new Color(0, 0, 0));
+		setBackground(new Color(247, 146, 69));
 		setDoubleBuffered(true);
 		setFocusable(true);
 		screenTracker = "title_screen";
+		
 		repaint();
 		
 	}
@@ -37,7 +48,7 @@ public class Panel extends JPanel implements KeyListener {
 	
 	public void drawSplashPage(Graphics g) {
 		//Title Screen//
-		g.setColor(Color.red);
+		g.setColor(Color.blue);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 90)); 
 		//Title//
 		g.drawString("Fidgy-Rubiks", 150, 250);
@@ -57,10 +68,10 @@ public class Panel extends JPanel implements KeyListener {
 			drawSplashPage(g);
 		else
 			SecondScreen(g);
+			//ThirdScreen(g);
+		    
 		
-		drawSplashPage(g);
 		
-		ThirdScreen(g);
 	}
 	
 	
@@ -68,27 +79,33 @@ public class Panel extends JPanel implements KeyListener {
 		//Rubiks Cube Menu//
 		
 		//Solve Mode//
-		g.setColor(Color.red);
+		g.setColor(Color.blue);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
 		g.drawString("Game Mode", 230, 50 );
 		
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30)); 
-
+		//////////////////////////////////////////////
 		g.drawString("Beginner", 300, 150);
 		
 		g.drawString("Intermediate", 300, 250);
 		
 		g.drawString("Profesional", 300, 350);
-		
+		//////////////////////////////////////////////
 		//Shopping Cart and Menu Bar//
-		g.drawImage(new ImageIcon("src/pictures/Shopping Cart 2.png").getImage(), 650, 10, 200, 100, null);
+		g.drawImage(new ImageIcon("src/pictures/Shopping Cart 2.png").getImage(), 650, 10, 100, 100, null);
 		g.drawImage(new ImageIcon("src/pictures/Menu Bar.png").getImage(), -50, 0, 150, 100, null);
+		//////////////////////////////////////////////
+		g.drawRect(300, 350, 100, 100);
+		
+		
+		
+		
 	}
 		
 	public void ThirdScreen(Graphics g) {
 		
 		//Easy Mode//
-		g.setColor(Color.red);
+		g.setColor(Color.blue);
 		g.setFont(new Font("TimesRoman", Font.PLAIN,30));
 		
 		
@@ -106,8 +123,11 @@ public class Panel extends JPanel implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent k) {
 		if(k.getKeyCode()==KeyEvent.VK_SPACE)
-		screenTracker="Selection_creeen";
-			
+		//screenTracker="Selection_screen";
+		screenListIndex++;
+		if(screenListIndex==Screen.length)
+			screenListIndex=0;
+		screenTracker=Screen[screenListIndex];
 			
 			
 	}
